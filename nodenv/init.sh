5ed9635bc572d8788752484338ac2544748a3658
Mattia Penati <mattia.penati@protonmail.com>
2020-12-28 00:23:04 +0100

    update

--- nodenv/init.sh
+++ nodenv/init.sh
@@ -11,3 +11,4 @@ popd
 
 mkdir -p $HOME/.nodenv/plugins
 git clone https://github.com/nodenv/node-build.git $HOME/.nodenv/plugins/node-build
+git clone https://github.com/pine/nodenv-yarn-install.git $HOME/.nodenv/plugins/nodenv-yarn-install"
