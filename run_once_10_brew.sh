1ae3eac3aba7ec23b8892730a1f49858c9b3758e
Mattia Penati <mattia.penati@protonmail.com>
2021-03-19 21:40:48 +0100

    homebrew

---
+++ run_once_10_brew.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -eu
+IFS=$'\n\t'
+
+
+case ${OSTYPE:-} in darwin*)
+    if command -v brew &> /dev/null; then
+        echo "homebrew already installed"
+    else
+        curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
+    fi
+
+    brew tap buo/cask-upgrade
+    brew install \
+         bandwhich \
+         bat \
+         chezmoi \
+         direnv \
+         fd \
+         fnm \
+         font-cascadia-mono-nf \
+         fzf \
+         git \
+         git-delta \
+         git-interactive-rebase-tool \
+         gitui \
+         htop \
+         jless \
+         lazygit \
+         lsd \
+         neovim \
+         node \
+         ripgrep \
+         starship \
+         stylua \
+         tmux \
+         topgrade
+esac
