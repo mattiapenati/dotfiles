4d43c464cb9b0d81879de2a92aa5f6bb68610435
Mattia Penati <mattia.penati@protonmail.com>
2021-06-05 16:14:01 +0200

    wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm';
+
+return {
+    color_scheme = "wezterm_tokyonight_storm",
+    hide_tab_bar_if_only_one_tab = true,
+    font = wezterm.font("JetBrainsMono Nerd Font"),
+    font_size = 12.0,
+    window_padding = {
+        left = 10,
+        right = 10,
+        top = 10,
+        bottom = 10,
+    },
+}
