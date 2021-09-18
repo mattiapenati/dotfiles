75c320700a5c418c042945c49a9caa2bc6f9eb83
Mattia Penati <mattia.penati@protonmail.com>
2021-09-18 21:00:17 +0200

    neovim -> lua

--- run_once_90_neovim.sh
+++ run_once_90_neovim.sh
@@ -2,14 +2,4 @@
 set -eu
 IFS=$'\n\t'
 
-[ -e ~/.local/share/nvim/site/autoload/plug.vim ] || \
-    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-COC_EXTENSIONS="coc-clangd coc-cmake coc-fzf-preview coc-json coc-pairs"
-COC_EXTENSIONS="$COC_EXTENSIONS coc-pyright coc-python coc-rust-analyzer"
-COC_EXTENSIONS="$COC_EXTENSIONS coc-toml coc-xml coc-yaml"
-
-npm install -g neovim
-nvim --headless -c PlugInstall -c qa
-nvim --headless -c "CocInstall -sync $COC_EXTENSIONS" -c qa
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
