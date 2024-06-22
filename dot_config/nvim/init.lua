af2a357f6722bfd9af7022e4e3ec41d022c17bdf
Mattia Penati <mattia.penati@protonmail.com>
2024-06-22 18:48:01 +0200

    neovim -> lazyvim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,2 @@
-local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
-    vim.cmd 'packadd packer.nvim'
-end
-
-require'autocommand'
-require'plugins'
-require'settings'
-require'autocomplete'
-require'lsp'
-require'term'
-require'theme'
-require'keymaps'
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
